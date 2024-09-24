f1ed12a0fe774cc897b64ce21cdf63c350a7d9e6
Andrei <andrebest94@gmail.com>
2024-09-24 11:31:04 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pom.xml
    #	src/main/java/org/example/App.java
    #	src/test/java/org/example/FirstTestCase.java

--- src/test/java/org/example/TC1.java
+++ src/test/java/org/example/TC1.java
@@ -1,11 +1,26 @@
 package org.example;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TC1 {
+    @BeforeClass
+    void beforeClass(){
+        System.out.println("This will be execute before the class");
+    }
+    @AfterClass
+    void afterClass(){
+        System.out.println("This will be execute after  the class");
+    }
+
+
+    @BeforeMethod
     void beforeMethod(){
         System.out.println("This will be execute before every method");
+    }
+
+    @AfterMethod
+    void afterMethod(){
+        System.out.println("This will be execute after every method");
     }
 
     @Test
@@ -16,5 +31,13 @@ public class TC1 {
     @Test
     void test2(){
         System.out.println("This is test 2");
+    }
+    @BeforeTest
+    void beforeTest(){
+        System.out.println("This will execute after the test");
+    }
+    @AfterTest
+    void afterTest(){
+        System.out.println("This will execute after the test");
     }
 }
